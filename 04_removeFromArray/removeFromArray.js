f4faef3f80c79de8c53a82925984483add9929f1
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-02-15 21:43:36 +0100

    Excercise 04 is done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valueToErase) 
+{
+    const newArray = [];
 
+    for (let index = 0; index < array.length; index++)
+    {
+        if (findFalseValue(array[index], valueToErase) === false)
+            newArray[newArray.length] = array[index];
+    }
+    return (newArray);
 };
 
+function findFalseValue(arrayToSearch, valuesToErase)
+{
+    for (let i = 0; i < valuesToErase.length; i++) 
+    {
+        if (arrayToSearch === valuesToErase[i])
+            return (true);
+    }
+    return (false);
+}
 // Do not edit below this line
 module.exports = removeFromArray;

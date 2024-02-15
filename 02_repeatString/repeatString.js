b73eee9ebc10873f27ca55f7fb339a49c32ef796
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-02-15 21:41:36 +0100

    Ex 02 is done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringInput, numberOfRepetions) 
+{
+    let stringResult = "";
+    if (numberOfRepetions < 0)
+        return ("ERROR");
+  for (let index = 0; index < numberOfRepetions; index++) 
+  {
+    stringResult += stringInput;
+  }
+  return (stringResult);
 };
 
 // Do not edit below this line

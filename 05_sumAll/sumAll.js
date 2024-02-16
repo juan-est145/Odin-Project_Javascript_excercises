56f67ba62a84a69089384c8437616ff2c58f60ba
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-02-16 16:20:30 +0100

    Excercise 5 almost done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,7 @@
 const sumAll = function(number1, number2) 
 {
-    if (number1 < 0 || number2 < 0 || Number.isInteger(number1) === false || Number.isInteger(number2) === false)
+    if (number1 < 0 || number2 < 0 || Number.isInteger(number1) === false 
+    || Number.isInteger(number2) === false)
         return ("ERROR");
     else if (number2 === number1 + 1)
         return (number1 + number2);

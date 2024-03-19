496fcf23a33efbcda40b3dd233a397e22c265505
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-03-19 20:20:40 +0100

    Excercise 10 is done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index == 1)
+        return (1);
+    else if (index == 0)
+        return (0);
+    else if (index < 0)
+        return ("OOPS");
+    return (fibonacci(index - 2) + fibonacci(index - 1));
 };
 
 // Do not edit below this line

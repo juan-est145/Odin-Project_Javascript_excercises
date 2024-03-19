960a1a5024a6971916905ca6242ee9cb3e8e2ec9
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-03-19 20:14:50 +0100

    Excercise 09 is done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toUpperCase();
+    for(let i = 0; i < string.length; i++)
+    {
+        //if (string[i] < 'A' || string[i] > 'Z')
+        if (!(string[i] >= 'A' && string[i] <= 'Z') && !(string[i] >= '0' && string[i] <= '9'))
+            string = string.replaceAll(string[i], '');
+    }
+    const reverseString = string.split('').reverse().join('');
+    return (string === reverseString? true : false);
 };
 
 // Do not edit below this line

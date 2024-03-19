d1890ad655bac873a73ab9a40e05420673c01d12
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-03-19 19:01:40 +0100

    Excercise 08 is done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return (number1 + number2);
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return (number1 - number2);
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  if (numbers.length === 0)
+    return (0);
+	return (numbers.reduce((total, current) => total + current));
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  if (numbers.length === 0)
+  return (0);
+return (numbers.reduce((total, current) => total * current));
 };
 
-const power = function() {
-	
+const power = function(number, exponent) {
+  let result = number;
+  for(let i = 1; i < exponent; i++)
+    result *= number;
+  return (result);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number <= 1)
+    return (1);
+  else
+    return (number * factorial(number - 1));
 };
 
 // Do not edit below this line

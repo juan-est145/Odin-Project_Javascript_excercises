2f50c985f9bf3cd4f85fe45767f787e8cc07cba9
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-03-20 11:49:01 +0100

    Excercise 12 is done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    return (peopleArray.reduce((oldest, currentPerson) => 
+    {
+        const oldestAge = "yearOfDeath" in oldest? oldest.yearOfDeath - oldest.yearOfBirth : new Date().getFullYear() - oldest.yearOfBirth;
+        const currentPersonAge = "yearOfDeath" in currentPerson? currentPerson.yearOfDeath - currentPerson.yearOfBirth : new Date().getFullYear() - currentPerson.yearOfBirth;
+        return (oldestAge > currentPersonAge ? oldest : currentPerson);
+    }))
 };
 
 // Do not edit below this line

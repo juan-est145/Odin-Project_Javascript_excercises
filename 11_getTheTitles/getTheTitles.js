67ebefe77b039199027c12becccae7eda5aa5137
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-03-20 10:57:42 +0100

    Excercise 11 is done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) 
+{
+    return (books.filter((object) => "title" in object).map((object) => object.title));
 };
+
+//Alternative solution
+/*const array = new Array;
+    books.forEach(element => {
+        if ("title" in element == true)
+            array.push(element.title);
+    });
+    return (array);*/
 
 // Do not edit below this line
 module.exports = getTheTitles;
